b0bf470a05fa63d06299330a195366cd067f28c6
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 16:05:40 -0500

    Clean URLs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
 import Home from './modules/Home';
 import About from './modules/About';
@@ -9,7 +9,7 @@ import Repo from './modules/Repo';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/about" component={About}/>
