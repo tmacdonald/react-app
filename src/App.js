8aaa8a99e008171b4ed052ba3f89235fa45e4656
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 15:51:31 -0500

    Active links

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
+          <li><Link to="/about" activeClassName="active">About</Link></li>
+          <li><Link to="/repos" activeClassName="active">Repos</Link></li>
         </ul>
 
         {this.props.children}
