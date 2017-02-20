2ddb65492565a7a748e5f74e8dbd210cd75b1f95
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 15:44:42 -0500

    More routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './App';
+import About from './modules/About';
+import Repos from './modules/Repos';
 import './index.css';
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+    <Route path="/repos" component={Repos}/>
   </Router>,
   document.getElementById('root')
 );
---
+++ src/modules/Repos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Repos extends Component {
+    render() {
+        return (
+            <div>Repos</div>
+        );
+    }
+}
+
+export default Repos;
