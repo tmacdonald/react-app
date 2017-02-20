46f2120e19f33d72f05535d114a91691955efa6e
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 16:01:37 -0500

    IndexRoute

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './App';
+import Home from './modules/Home';
 import About from './modules/About';
 import Repos from './modules/Repos';
 import Repo from './modules/Repo';
@@ -10,6 +11,7 @@ import './index.css';
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/repos" component={Repos}>
         <Route path="/repos/:userName/:repoName" component={Repo}/>
