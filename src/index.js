c25b5aaafefa8936079010347c8a2e709dd8f8ec
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 15:40:40 -0500

    Added first route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>,
   document.getElementById('root')
 );
