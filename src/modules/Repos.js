b357ff2442a945a83fbc2076042e5aca7106a6a8
Tim Macdonald <macdonald.tim@gmail.com>
2017-02-20 22:02:30 -0500

    Navigating Programmatically

--- src/modules/Repos.js
+++ src/modules/Repos.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 
 class Repos extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+        const userName = event.target.elements[0].value;
+        const repo = event.target.elements[1].value;
+        const path = `/repos/${userName}/${repo}`;
+        browserHistory.push(path);
+    }
     render() {
         return (
             <div>
@@ -10,6 +17,13 @@ class Repos extends Component {
                 <ul>
                     <li><Link to="/repos/reactjs/react-router" activeClassName="active">React Route</Link></li>
                     <li><Link to="/repos/facebook/react" activeClassName="active">React</Link></li>
+                    <li>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" placeholder="userName"/> / {' '}
+                            <input type="text" placeholder="repo"/> / {' '}
+                            <button type="submit">Go</button>
+                        </form>
+                    </li>
                 </ul>
 
                 {this.props.children}
